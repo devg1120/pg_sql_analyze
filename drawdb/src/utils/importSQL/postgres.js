dc4b154e03478fe93357f7ee591ec79f78fc7aef
devg1120 <devg1120@gmail.com>
2024-09-21 17:57:04 +0900

    up

--- drawdb/src/utils/importSQL/postgres.js
+++ drawdb/src/utils/importSQL/postgres.js
@@ -255,11 +255,16 @@ export function fromPostgres(ast, diagramDb = DB.GENERIC) {
         ) {
           const relationship = {};
           const startTable = e.table[0].table;
-          const startField = expr.create_definitions.definition[0].column;
+          const startField = expr.create_definitions.definition[0].column["expr"]["value"];
           const endTable =
             expr.create_definitions.reference_definition.table[0].table;
           const endField =
-            expr.create_definitions.reference_definition.definition[0].column;
+            expr.create_definitions.reference_definition.definition[0].column["expr"]["value"];
+
+          //console.log("startTable",startTable);
+          //console.log("endTable",endTable);
+          //console.log("startField",startField);
+          //console.log("endField",endField);
           let updateConstraint = "No action";
           let deleteConstraint = "No action";
           expr.create_definitions.reference_definition.on_action.forEach(
@@ -284,12 +289,12 @@ export function fromPostgres(ast, diagramDb = DB.GENERIC) {
           const endTableId = tables.findIndex((t) => t.name === endTable);
           if (endTableId === -1) return;
 
-          const endFieldId = tables[endTableId].fields.findIndex(
+          let endFieldId = tables[endTableId].fields.findIndex(
             (f) => f.name === endField,
           );
-          if (endField === -1) return;
+          if (endField == -1) return;
 
-          const startFieldId = tables[startTableId].fields.findIndex(
+          let startFieldId = tables[startTableId].fields.findIndex(
             (f) => f.name === startField,
           );
           if (startFieldId === -1) return;
